bcd77c8bafff81c419f62ba28cc3e42e9459c819
kevin cuadros <kevindavidcuadros@gmail.com>
2025-01-18 16:05:03 -0500

    create toolbar

---
+++ ChickenAppFrisby/Components/NavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBar.swift
+//  ChickenAppFrisby
+//
+//  Created by Kevin Cuadros on 18/01/25.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBar()
+}
